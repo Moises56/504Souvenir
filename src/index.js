6d6693c5e4e4cb4223e6162f4e329a3a60daf7fa
Moises56 <moiavil56@gmail.com>
2020-10-14 11:54:47 -0600

    Version:1.2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'produccion'){
+if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config(); //*asigna a la variables de entorno
     //console.log(process.env.NODE_ENV)
 
