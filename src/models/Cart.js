7454170fb9ac95b167503e7155c9ee13680975c7
THEOCKLES <theockles@THEOCKLES.TEO.IP>
2020-12-01 00:52:08 -0600

    version 2.1

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,66 +1,55 @@
 module.exports = function Cart(cart) {
-    this.items = cart.items || {};
-    this.totalItems = cart.totalItems || 0;
-    // this.numItems = cart.numItems || 0;
-    this.totalPrice = cart.totalPrice || 0;
-    this.totalQty = cart.totalQty || 0;
+  this.items = cart.items || {};
+  //   this.totalQty = cart.totalQty || 0;
+  // this.numItems = cart.numItems || 0;
+  this.totalPrice = cart.totalPrice || 0;
+  this.totalQty = cart.totalQty || 0;
 
-    this.add = function(item, id) {
-        var cartItem = this.items[id];
-        if (!cartItem) {
-            cartItem = this.items[id] = {item: item, quantity: 0, precio: 0};
-        }
-        cartItem.quantity++;
-        cartItem.precio = cartItem.item.precio * cartItem.quantity;
-        this.totalItems++;
-        this.totalPrice += cartItem.item.precio;
-    };
-
-      this.removeItem = function(id) {
-        this.totalItems -= this.items[id].quantity;
-        this.totalPrice -= this.items[id].precio;
-        // this.items[id].quantity <= 0;
-        delete this.items[id];
-        
-      };
-    this.getItems = function() {
-        var arr = [];
-        for (var id in this.items) {
-            arr.push(this.items[id]);
-        }
-        return arr;
-    };
-
-    this.reduceItem = (id) => {
-       this.totalItems -= this.items[id].quantity--;
-        this.items[id].quantity--;
-        this.items[id].precio -= this.items[id].item.precio;
-        this.totalQty--;
-        this.totalPrice -= this.items[id].item.precio;
-        
-        
-        if (this.items[id].quantity <= 0) {
-            // this.totalItems -= this.items[id].quantity;
-            delete this.items[id];
-        }
+  this.add = function (item, id) {
+    var cartItem = this.items[id];
+    if (!cartItem) {
+      cartItem = this.items[id] = { item: item, quantity: 0, precio: 0 };
     }
+    cartItem.quantity++;
+    cartItem.precio = cartItem.item.precio * cartItem.quantity;
+    this.totalQty++;
+    this.totalPrice += cartItem.item.precio;
+  };
 
-    this.aumentaItem = (id) => {
-        this.totalItems += this.items[id].quantity;
-        this.items[id].quantity++;
-        this.items[id].precio += this.items[id].item.precio;
-        this.totalQty++;
-        this.totalPrice += this.items[id].item.precio;
-        
-        
+  this.getItems = function () {
+    var arr = [];
+    for (var id in this.items) {
+      arr.push(this.items[id]);
     }
+    return arr;
+  };
 
+  this.removeItem = (id) => {
+    this.totalQty -= this.items[id].quantity;
+    this.totalPrice -= this.items[id].precio;
+    delete this.items[id];
+  };
+
+  this.reduceItem = (id) => {
+    this.items[id].quantity--;
+    this.items[id].precio -= this.items[id].item.precio;
+    this.totalQty--;
+    this.totalPrice -= this.items[id].item.precio;
+
+    if (this.items[id].quantity <= 0) {
+      delete this.items[id];
+    }
+  };
+
+  this.aumentaItem = (id) => {
+    this.items[id].quantity++;
+    this.items[id].precio += this.items[id].item.precio;
+    this.totalQty++;
+    this.totalPrice += this.items[id].item.precio;
+  };
 };
 
-
 //************************************* */
-
-
 
 // module.exports = function Cart(oldCart) {
 //     this.items = oldCart.items || {};
