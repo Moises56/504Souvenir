1203c87f445df8c141f799a6bad4d0dda9045f8f
THEOCKLES <theockles@THEOCKLES.TEO.IP>
2020-12-14 03:05:50 -0600

    version 2.4

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -24,12 +24,7 @@ router.post("/notes/new-note", isAuthenticated, createNewNote);
 
 //*Obtenet todas las notas
 router.get("/notes", isAuthenticated, renderNotas);
-// router.get('/productos/new-productos', renderProductos)
-// router.get('index', renderProductosIndex)
 
-//*Edit notas
-//router.get('/notes/edit/:id', renderEditForm) //*Mostrar el formulario envia los datos
-//router.put('/notes/edit/:id', updateNotes) //*Autualizar los datos resive los datos
 
 router.get("/notes/edit/:id", isAuthenticated, renderEditForm);
 router.put("/notes/edit-note/:id", isAuthenticated, updateNotes);
@@ -42,46 +37,7 @@ router.get("/note/:id", (req, res) => {
 });
 
 //*Eliminar
-//router.delete('/notes/delete/:id', deleteNotes)
 router.delete("/notes/delete/:id", isAuthenticated, deleteNotes);
-
-// //?Estatus
-// router.get("/notes/status", isAuthenticated, async (req, res) => {
-//   const orders = await Order.find({ user: req.user })
-//     // const note = await Order.find({ note: req.user.id })
-//     .sort({ createdAt: -1 })
-//     .lean();
-//   // res.render('notes/status', {orders})
-//   // console.log(orders)
-//   // console.log(req.note.id )
-//   // var cart;
-//   //   orders.forEach(order => {
-//   //     cart = new Cart(order.cart);
-//   //     order.items = cart;
-//   //   });
-//   res.render("notes/status", { orders });
-// });
-
-// router.post('/notes/status',isAuthenticated, status)
-
-// notesCtrl.status = async (req, res) => {
-//     const orders = await Order.find({ 'user': req.user}, (err, orders) => {
-//       if(err){
-//         return res.write('Error');
-//       }
-//       var cart;
-//       orders.forEach(order => {
-//         cart = new Cart(order.cart);
-//         order.items = cart.generateArray();
-//       });
-//       res.render('notes/status', {orders});
-//     })
-
-//     // res.redirect("notes/status");
-//   }
-
-//?buscador
-// router.get('store/checkout', buscador)
 
 //TODO get ->Obtener
 //TODO post ->Crear
